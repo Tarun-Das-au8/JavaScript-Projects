7520eb9ee3cf649d4f57f72cb7a5274bb884c06e
Tarun-Das-au8 <imtarun79@gmail.com>
2021-04-01 10:46:15 +0530

    js file

---
+++ 3-Reviews/app.js
@@ -0,0 +1,102 @@
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const email = document.getElementById("email");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0
+
+var baseURL = 'https://reqres.in/api/users?page=2';
+function fetchPost(){
+  var result = fetch(`${baseURL}`,{
+    method: "GET"
+  }).then(function(res){
+    return res.json();
+  }).then(function(result){
+    return result.data;
+  }).catch(function(err){
+    console.log(err);
+  })
+  return result;
+}
+
+fetchPost().then(function(data){
+  
+  console.log(data);
+
+  window.addEventListener("DOMContentLoaded", function(){
+    showPerson()
+  })
+  
+  function showPerson(){
+    img.src = data[currentItem].avatar;
+    author.textContent = data[currentItem].first_name;
+    email.textContent = data[currentItem].email;
+  }
+  
+  nextBtn.addEventListener("click", function(){
+    currentItem++
+    if(currentItem > data.length-1){
+      currentItem = 0
+    }
+    showPerson()
+  })
+  
+  prevBtn.addEventListener("click", function(){
+    currentItem--
+    if(currentItem < 0){
+      currentItem = data.length-1
+    }
+    showPerson()
+  })
+  
+  randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * data.length);
+    showPerson()
+  })
+
+}).catch(function(err){
+  console.log(err);
+})
+
+// const reviews = [
+//   {
+//     id: 1,
+//     name: "susan smith",
+//     job: "web developer",
+//     img:
+//       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+//     text:
+//       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+//   },
+//   {
+//     id: 2,
+//     name: "anna johnson",
+//     job: "web designer",
+//     img:
+//       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+//     text:
+//       "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+//   },
+//   {
+//     id: 3,
+//     name: "peter jones",
+//     job: "intern",
+//     img:
+//       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+//     text:
+//       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+//   },
+//   {
+//     id: 4,
+//     name: "bill anderson",
+//     job: "the boss",
+//     img:
+//       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+//     text:
+//       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+//   },
+// ];
